363435ec5c37349f4511b720446cf1458b67b73d
vasiliy <fgh.detkov@gmail.com>
2026-03-26 23:34:20 +0300

    Updates

--- _next/static/lmqtjFTM18ouvgbcVIyLQ/_buildManifest.js
+++ _next/static/lmqtjFTM18ouvgbcVIyLQ/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/gardens//_next/:path+",
+        "destination": "/gardens/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
